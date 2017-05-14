2389864c8adb533e6e00b8e4e67942ca958ed471
blackhatt <munteanbogdan97@gmail.com>
2017-05-14 11:55:38 +0200

    Added the DBConn and the mysql path. we can put it in the gitignore later xD

---
+++ mainprojecttesting/src/DBConn.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConn {
+
+    private static final String URL = "jdbc:mysql://fypanm.c8w1xrl7tum6.eu-central-1.rds.amazonaws.com:3306/";
+    private static final String DB_NAME = "motorhome_system";
+    private static final String USER = "mhadmin";
+    private static final String PASS = "firstyearkea2k17";
+
+
+    public static Connection getConn() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(
+                    DBConn.URL + DBConn.DB_NAME,
+                    DBConn.USER,
+                    DBConn.PASS);
+            return conn;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public static String getDbName() {
+        return DB_NAME;
+    }
+}
+
