2eb2e8269bb88565abb82f6cbd5011656b965df8
CIA <CIA@CIA.CIA>
2017-05-19 13:18:15 +0200

    datepicker kinda working, need to get range

--- mainprojecttesting/src/Controller/ReservationViewController.java
+++ mainprojecttesting/src/Controller/ReservationViewController.java
@@ -1,10 +1,42 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.input.MouseEvent;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  * Created by blackhatt on 16/05/2017.
  */
 public class ReservationViewController {
 
+    @FXML DatePicker reservationPicker;
+    ArrayList<LocalDate> range = new ArrayList<>();
+    boolean dateButtonClicked = false;
+    @FXML public void initialize(){
 
+        reservationPicker.setOnAction(event -> {
+            if (dateButtonClicked){
+                dateButtonClicked = false;
+            }
+            LocalDate date = reservationPicker.getValue();
+            System.out.println("Selected date: " + date);
 
+        });
+        reservationPicker.setOnHidden(event -> {
+            if(!dateButtonClicked){
+                reservationPicker.show();
+                dateButtonClicked = true;
+            }
+        });
+    }
+
+    public void chooseDate(MouseEvent mouseEvent) {
+
+//        LocalDate date = reservationPicker.getValue();
+//        System.out.println(date.toString());
+
+    }
 }
